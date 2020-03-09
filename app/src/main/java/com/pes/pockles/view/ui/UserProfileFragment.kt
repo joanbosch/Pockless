703b2734ac5cb329dfd6330f69125913e4d668d6
Sergio Vázquez <sergiovazquezmontesdeoca@gmail.com>
2020-03-09 10:23:37 +0100

    Project structure

--- app/src/main/java/com/pes/pockles/view/ui/UserProfileFragment.kt
+++ app/src/main/java/com/pes/pockles/view/ui/UserProfileFragment.kt
@@ -1,4 +1,26 @@
 package com.pes.pockles.view.ui
 
-class UserProfileFragment {
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+import com.pes.pockles.R
+
+/**
+ * A simple [Fragment] subclass.
+ */
+class UserProfileFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_user_profile, container, false)
+    }
+
+
 }
