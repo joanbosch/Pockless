66f653913dc33877c38948357d2f85e28daade99
Sergio Vázquez <sergiovazquezmontesdeoca@gmail.com>
2020-03-15 12:02:17 +0100

    Finished Menu. New Pock Activity first commit.

--- app/src/main/java/com/pes/pockles/view/ui/MainActivity.kt
+++ app/src/main/java/com/pes/pockles/view/ui/MainActivity.kt
@@ -2,13 +2,25 @@ package com.pes.pockles.view.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import androidx.drawerlayout.widget.DrawerLayout
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
+import androidx.navigation.findNavController
+import androidx.navigation.ui.NavigationUI
 import com.pes.pockles.R
+import com.pes.pockles.databinding.ActivityMainBinding
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+
+        var navController = findNavController(R.id.navigationHostFragment)
+        NavigationUI.setupWithNavController(binding.bottomNavigation, navController)
 
     }
 }
