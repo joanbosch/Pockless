9abd00257b8ce3a703bee90d6d087c8db8ee40d7
inchko <inchko@gmail.com>
2020-03-21 18:24:37 +0100

    Permission management/get users location (doesn't work yet)

--- app/src/main/java/com/pes/pockles/view/ui/MapFragment.kt
+++ app/src/main/java/com/pes/pockles/view/ui/MapFragment.kt
@@ -1,48 +1,131 @@
 package com.pes.pockles.view.ui
 
 
-import android.app.Activity
+import android.content.pm.PackageManager
+import android.location.Location
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.annotation.NonNull
+import androidx.annotation.Nullable
+import androidx.core.app.ActivityCompat
 import androidx.fragment.app.Fragment
-import androidx.fragment.app.FragmentActivity
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationServices
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MarkerOptions
+import com.google.android.gms.tasks.OnFailureListener
+import com.google.android.gms.tasks.OnSuccessListener
 import com.pes.pockles.R
-import kotlinx.android.synthetic.main.fragment_map.view.*
 
 
 /**
  * A [Fragment] subclass for map view.
  */
 class MapFragment : Fragment() , OnMapReadyCallback {
-    private lateinit var mMap: GoogleMap
+   @Nullable private var mMap: GoogleMap?=null
+    private lateinit var lastLocation: Location
+    private lateinit var fusedLocationClient: FusedLocationProviderClient
+
+
+
+    /*   override fun onCreateView(
+           inflater: LayoutInflater, container: ViewGroup?,
+           savedInstanceState: Bundle?
+       ): View? {
+           initMap()
+
+           return inflater.inflate(R.layout.fragment_map, container, false)
+       }*/
 
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
+        inflater: LayoutInflater,
+        container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        val mapFragment: SupportMapFragment? = childFragmentManager.findFragmentById(R.id.mapview) as SupportMapFragment?
-        mapFragment?.getMapAsync(this)
-        return inflater.inflate(R.layout.fragment_map, container, false)
-
+        val rootView: View =
+            inflater.inflate(R.layout.fragment_map, container, false)
+        val mapFragment =
+            childFragmentManager.findFragmentById(R.id.mapView) as SupportMapFragment?
+        mapFragment!!.getMapAsync(this)
+        return rootView
     }
-
-
-
+/*
+    private fun initMap(){
+        if(mMap==null) {
+            val mapFragment: SupportMapFragment? =
+                childFragmentManager.findFragmentById(R.id.mapView) as SupportMapFragment?
+            mapFragment?.getMapAsync(this)
+        }
+    }*/
     override fun onMapReady(googleMap: GoogleMap) {
         mMap = googleMap
 
         // Add a marker in Sydney and move the camera
-        val sydney = LatLng(-34.0, 151.0)
-        mMap.addMarker(MarkerOptions().position(sydney).title("MyNewMarker"))
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
+        val loctest = LatLng(-30.0, 151.0)
+        mMap!!.addMarker(MarkerOptions().position(loctest).title("test"))
+      //  mMap!!.moveCamera(CameraUpdateFactory.newLatLngZoom(sydney, 12.0f))
+       setUpMap(mMap!!)
     }
 
+/* Permission and location management **/
+
+    companion object {
+        private const val LOCATION_PERMISSION_REQUEST_CODE = 1
+    }
+
+
+
+    private fun setUpMap( googleMap: GoogleMap) {
+        mMap = googleMap
+            if (context?.let {
+                    ActivityCompat.checkSelfPermission(
+                        it,
+                        android.Manifest.permission.ACCESS_FINE_LOCATION)
+                } != PackageManager.PERMISSION_GRANTED &&
+                context?.let {
+                    ActivityCompat.checkSelfPermission(
+                        it,
+                        android.Manifest.permission.ACCESS_COARSE_LOCATION)
+                } != PackageManager.PERMISSION_GRANTED) {
+                this.activity?.let {
+                    ActivityCompat.requestPermissions(
+                        it,
+                        arrayOf(android.Manifest.permission.ACCESS_FINE_LOCATION), LOCATION_PERMISSION_REQUEST_CODE)
+                }; // return
+                mMap!!.isMyLocationEnabled = true
+
+
+                getLastLocation()
+
+                        val currentLatLng = LatLng(lastLocation.latitude, lastLocation.longitude)
+                        mMap!!.animateCamera(CameraUpdateFactory.newLatLngZoom(currentLatLng, 12f))
+                    }
+                }
+    private fun getLastLocation() {
+        val locationClient = LocationServices.getFusedLocationProviderClient(activity!!)
+        try
+        {
+            locationClient.lastLocation
+                .addOnSuccessListener { location -> // GPS location can be null if GPS is switched off
+                    if (location != null) {
+                        if (mMap != null) {
+                            mMap!!.moveCamera(CameraUpdateFactory.newLatLngZoom(LatLng(location.latitude, location.longitude), 15f))
+                        }
+                    }
+                }
+                .addOnFailureListener { e ->
+                    e.printStackTrace()
+                }
+        }
+        catch (e:SecurityException) {
+            e.printStackTrace()
+        }
+    }
 }
