e75a2487444b6bb603ef893fae78ec55a146e9d2
Victor <victorblancogarcia3@gmail.com>
2020-03-30 17:41:22 +0200

    Revert "fix conflicts"
    This reverts commit 5ded1273

--- app/src/main/java/com/pes/pockles/view/ui/newpock/NewPockViewModel.kt
+++ app/src/main/java/com/pes/pockles/view/ui/newpock/NewPockViewModel.kt
@@ -7,48 +7,37 @@ import androidx.lifecycle.ViewModel
 import com.pes.pockles.data.Resource
 import com.pes.pockles.domain.usecases.NewPockUseCase
 import com.pes.pockles.model.Location
-import com.pes.pockles.model.NewPock
 import com.pes.pockles.model.Pock
 import com.pes.pockles.util.AbsentLiveData
 
 class NewPockViewModel : ViewModel() {
-    private val _errorHandler = MutableLiveData<Boolean>()
-    private val _chatEnabled = MutableLiveData<Boolean>()
-    private val _pockToInsert = MutableLiveData<NewPock?>()
-    val pockContent = MutableLiveData<String>()
-    val pockCategory = MutableLiveData<String>()
 
     private val useCase: NewPockUseCase by lazy {
         NewPockUseCase()
     }
 
-    val networkCallback: LiveData<Resource<Pock>?>
-        get() = Transformations.switchMap(_pockToInsert) { value: NewPock? ->
+    private val _pockToInsert = MutableLiveData<Pock?>()
+    val networkCallback: LiveData<Resource<Pock>>
+        get() = Transformations.switchMap(_pockToInsert) { value: Pock? ->
             if (value != null) useCase.execute(value) else AbsentLiveData.create()
         }
 
-    val errorHandlerCallback: LiveData<Boolean>
-        get() = _errorHandler
+    val pockTitle = MutableLiveData<String>()
+    val pockContent = MutableLiveData<String>()
+    val pockCategory = MutableLiveData<String>()
+    val chatEnabled = MutableLiveData<Boolean>()
 
     init {
-        _chatEnabled.value = false
+        chatEnabled.value = false
     }
 
     fun insertPock() {
-        val category: String = if (pockCategory.value == null)
-            "General"
-        else pockCategory.value.toString()
-
-        if (pockContent.value == null)
-            _errorHandler.value = true
-        else {
-            _pockToInsert.value = NewPock(
-                message = pockContent.value!!,
-                category = category,
-                chatAccess = _chatEnabled.value!!,
-                location = Location(0f, 0f) // obtain current location
-            )
-        }
+        _pockToInsert.value = Pock(
+            message = pockContent.value!!,
+            category = pockCategory.value!!,
+            chatAccess = chatEnabled.value!!,
+            location = Location(0.0, 0.0) // obtain current location
+        )
     }
 
 }
