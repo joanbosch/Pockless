9df80d929118b662b7b45fd49c60b237b55327d4
Sergio Vázquez <sergiovazquezmontesdeoca@gmail.com>
2020-05-01 18:05:08 +0200

    Added fcm token manager

--- app/src/main/java/com/pes/pockles/data/messaging/Notification.kt
+++ app/src/main/java/com/pes/pockles/data/messaging/Notification.kt
@@ -1,5 +1,36 @@
 package com.pes.pockles.data.messaging
 
 enum class Notification {
-    CHAT, LIKE, REPORT
+    CHAT() {
+        override fun onMessageRecieved() {
+            TODO("not implemented")
+        }
+
+    },
+    TRENDING() {
+        override fun onMessageRecieved() {
+            TODO("not implemented")
+        }
+
+    },
+    REPORTS() {
+        override fun onMessageRecieved() {
+            TODO("not implemented")
+        }
+
+    },
+    ACHIEVEMENT() {
+        override fun onMessageRecieved() {
+            TODO("not implemented")
+        }
+
+    },
+    BAN() {
+        override fun onMessageRecieved() {
+            TODO("not implemented")
+        }
+
+    };
+
+    abstract fun onMessageRecieved()
 }
--- app/src/main/java/com/pes/pockles/data/messaging/PocklesMessagingService.kt
+++ app/src/main/java/com/pes/pockles/data/messaging/PocklesMessagingService.kt
@@ -1,7 +1,18 @@
 package com.pes.pockles.data.messaging
 
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.app.PendingIntent
+import android.content.Context
+import android.content.Intent
+import android.media.RingtoneManager
+import android.os.Build
+import androidx.core.app.NotificationCompat
 import com.google.firebase.messaging.FirebaseMessagingService
 import com.google.firebase.messaging.RemoteMessage
+import com.pes.pockles.R
+import com.pes.pockles.data.FCMTokenManager
+import com.pes.pockles.view.ui.MainActivity
 
 class PocklesMessagingService : FirebaseMessagingService() {
 
@@ -12,11 +23,12 @@ class PocklesMessagingService : FirebaseMessagingService() {
      */
     override fun onNewToken(token: String) {
         //Send token to PAPI
-        sendRegistrationToServer(token)
+
     }
 
     override fun onMessageReceived(remoteMessage: RemoteMessage) {
         remoteMessage.data.get("mensaje")
+        sendNotification("Viva España", "VIVA FRANCO")
         when(remoteMessage.messageType){
 
         }
@@ -28,5 +40,40 @@ class PocklesMessagingService : FirebaseMessagingService() {
 
     private fun sendRegistrationToServer(token: String){
         //TODO
+    }
+
+    private fun sendNotification(messageBody: String, title: String) {
+        val intent = Intent(this, MainActivity::class.java)
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP)
+        val pendingIntent = PendingIntent.getActivity(this, 0 /* Request code */, intent,
+            PendingIntent.FLAG_ONE_SHOT)
+
+        val channelId = "PocklesChanel"
+        val defaultSoundUri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION)
+        val notificationBuilder = NotificationCompat.Builder(this, channelId)
+            .setSmallIcon(R.drawable.ic_pock_icon)
+            .setContentTitle(title)
+            .setContentText(messageBody)
+            .setAutoCancel(true)
+            .setSound(defaultSoundUri)
+            .setContentIntent(pendingIntent)
+
+        val notificationManager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+
+        // Since android Oreo notification channel is needed.
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val channel = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                NotificationChannel(
+                    channelId,
+                    "Channel human readable title",
+                    NotificationManager.IMPORTANCE_DEFAULT
+                )
+            } else {
+                TODO("VERSION.SDK_INT < O")
+            }
+            notificationManager.createNotificationChannel(channel)
+        }
+
+        notificationManager.notify(0 /* ID of notification */, notificationBuilder.build())
     }
 }
