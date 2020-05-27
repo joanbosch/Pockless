67af4caeafc5e718e2155758cf8e2859d4fae51a
Joan <Joan@DESKTOP-DQID8I5>
2020-05-27 14:14:13 +0200

    Errors fixed

--- app/src/main/java/com/pes/pockles/di/modules/ActivitiesModule.kt
+++ app/src/main/java/com/pes/pockles/di/modules/ActivitiesModule.kt
@@ -1,6 +1,7 @@
 package com.pes.pockles.di.modules
 
 import com.pes.pockles.view.ui.MainActivity
+import com.pes.pockles.view.ui.aboutus.AboutUsActivity
 import com.pes.pockles.view.ui.chat.ChatActivity
 import com.pes.pockles.view.ui.editpock.EditPockActivity
 import com.pes.pockles.view.ui.editprofile.EditProfileActivity
@@ -10,7 +11,9 @@ import com.pes.pockles.view.ui.login.register.RegisterActivity
 import com.pes.pockles.view.ui.login.register.RegisterActivityIcon
 import com.pes.pockles.view.ui.newpock.NewPockActivity
 import com.pes.pockles.view.ui.pockshistory.PocksHistoryActivity
+import com.pes.pockles.view.ui.settings.SettingsActivity
 import com.pes.pockles.view.ui.viewpock.ViewPockActivity
+import com.pes.pockles.view.ui.viewuser.ViewUserActivity
 import dagger.Module
 import dagger.android.ContributesAndroidInjector
 
