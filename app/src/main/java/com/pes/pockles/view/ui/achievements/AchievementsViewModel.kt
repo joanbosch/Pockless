bcdad7ab1e655c8c16e577e83ec70255460a068c
Vmorais22 <victormoraisllahi@gmail.com>
2020-05-29 12:19:17 +0200

    Doing achievemenetsViewModel and api call (not finished)

--- app/src/main/java/com/pes/pockles/view/ui/achievements/AchievementsViewModel.kt
+++ app/src/main/java/com/pes/pockles/view/ui/achievements/AchievementsViewModel.kt
@@ -1,5 +1,27 @@
 package com.pes.pockles.view.ui.achievements
 
-class AchievementsViewModel {
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MediatorLiveData
+import androidx.lifecycle.ViewModel
+import com.pes.pockles.data.Resource
+import com.pes.pockles.data.loading
+import com.pes.pockles.data.repository.AchievementsRepository
+import com.pes.pockles.model.Achievement
+import javax.inject.Inject
 
-}
+class AchievementsViewModel @Inject constructor(
+        private var repository: AchievementsRepository
+    ) : ViewModel() {
+        val notifications: LiveData<Resource<List<Achievement>>>
+            get() = _notifications
+
+        private val _notifications = MediatorLiveData<Resource<List<Achievement>>>()
+
+        fun refreshNotifications() {
+                val data = repository.getNotifications()
+                _notifications.addSource(data) {
+                        _notifications.value = it
+                        if (!it.loading) _notifications.removeSource(data)
+                   }
+            }
+    }
